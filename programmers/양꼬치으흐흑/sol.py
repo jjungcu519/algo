fc0f45b9b4b17faced8d7096923e5cf9963cd392
jjungcu519 <jjungcu0519@gmail.com>
2024-06-27 16:45:29 +0900

    programmers_3

---
+++ programmers/양꼬치으흐흑/sol.py
@@ -0,0 +1,12 @@
+def solution(n, k):
+
+    answer = 0
+    #n: 양꼬치 몇인분 / k: 음료수 개수
+    
+    
+    answer = (12000 * n) + (2000 * k) - (2000 * n//10)
+    
+    return answer
+
+
+print(solution(64,6))
